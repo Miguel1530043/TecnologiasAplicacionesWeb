fc6b86063ecac992fa58c0b564a5b84e62a05e34
Miguel1530043 <32343052+Miguel1530043@users.noreply.github.com>
2018-05-12 13:22:42 -0500

    Practica06

---
+++ Practica06/Ejercicio1/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Curso PHP |  Bienvenidos</title>
+    <link rel="stylesheet" href="./css/foundation.css" />
+    <script src="./js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+<div class="row">
+      <div class="large-6 columns">
+        <h2 style="color:#03AFCD">Practica N°6 | Ejercicio N°1</h2>
+      </div>
+      <div class="large-6 columns">
+        <ul class="right button-group">
+          <li><a href="../index.php" class="button" style="border-radius:10px">Inicio</a></li>
+          <li><a href="../Ejercicio2/index.php" class="button" style="border-radius:10px">Ejercicion N°2</a></li>
+        </ul>
+      </div>
+    </div>
---
+++ Practica06/Ejercicio2/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Curso PHP |  Bienvenidos</title>
+        <link rel="stylesheet" href="../css/foundation.css" />
+        <script src="../js/vendor/modernizr.js"></script>
+    </head>
+    <body>
+    
+    <?php require_once('header.php'); ?>
+
+     
+        <div class="row">
+            <div class="large-12 columns">
+                <div class="section-container tabs" data-section align="center">
+                    <section class="section">
+                        <div class="content" data-slug="panel1">
+                            <h2>Equipos Deportivos UPV</h2>
+                            <a href="football.php" class="button success" style="height:70px;width:300px;border-radius:10px;font-size:40px">FootBall</a><br>
+                            <a href="basketball.php" class="button success" style="height:70px;width:300px;border-radius:10px;font-size:40px">BasketBall</a>                
+                        </div>
+                    </section>
+                </div>
+            </div>
+
+        </div>
+    
+
+    <?php require_once('footer.php'); ?>
