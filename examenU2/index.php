54a72e0593645f8f6d6ce9fa6a56ff743b83855e
Miguel1530043 <32343052+Miguel1530043@users.noreply.github.com>
2018-06-26 05:24:03 -0500

    .

--- examenU2/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	require_once "models/links.php";
-	require_once "models/crud.php";
-	require_once "controllers/controller.php";
-
-	$MVC = new MvcController();//Instancia de la clase MvcController
-	$MVC -> page();//Ejecucion del metodo para desplegar las paginas
-?>
--- examenU2/models/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class Connection{
-		public static function connect(){
-			$db = new PDO("mysql:host=localhost;dbname=examenu2","root","");
-			return $db;
-		}
-	}
-
-?>
